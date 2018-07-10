92cfec02f41593a4fd4ed997bc456383135d4ae8
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-07-10 17:19:40 -0300

    webpack

--- curso-react-redux-master/exercicios_webpack/webpack.config.js
+++ curso-react-redux-master/exercicios_webpack/webpack.config.js
@@ -3,13 +3,13 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './ex/index.js',
+    entry: './ex.7/index.js',
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
     },
     devServer: {
-        port: 8080,
+        port: 8081,
         contentBase: './public'
     },
     // ExtractTextPlugin foi adicionado no ex.10
