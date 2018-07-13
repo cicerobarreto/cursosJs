c2c39834e5b9abcaf04d9593c0cf4cf0f37667b9
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-07-13 18:31:22 -0300

    continuação

--- curso-react-redux-master/todo-app-meu/frontend/src/todo/todo.jsx
+++ curso-react-redux-master/todo-app-meu/frontend/src/todo/todo.jsx
@@ -1,16 +1,50 @@
-    import React, { Component } from 'react'
-    import PageHeader from '../template/pageheader'
-    import TodoForm from './todoForm'
-    import TodoList from './todoList'
+import React, { Component } from 'react'
+import Axios from 'axios'
 
-    export default class Todo extends Component {
-        render() {
-            return (
-                <div>
-                    <PageHeader name='Tarefas' small='Cadastro' />
-                    <TodoForm />
-                    <TodoList />
-                </div>
-            )
-        }
+import PageHeader from '../template/pageheader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todo'
+
+export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChance = this.handleChance.bind(this)
+        this.refresh = this.refresh.bind(this)
+        this.state = { description: '', list: [] }
+        this.refresh()
     }
+
+    refresh(){
+        Axios.get(`${URL}?sort=-createdAt`)
+            .then( resp => {
+                this.setState({...this.state, list: resp.data})
+            })
+    }
+
+    handleChance(e) {
+        this.setState({ ...this.state, description: e.target.value })
+
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        Axios.post(URL, { description })
+            .then(resp => console.log(resp.statusText))
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm handleAdd={this.handleAdd}
+                    handleChance={this.handleChance}
+                    description={this.state.description} />
+                <TodoList />
+            </div>
+        )
+    }
+}
