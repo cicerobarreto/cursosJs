6ccca46c71ce94f996d50deb294e1b5a369881f0
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-07-17 11:06:52 -0300

    Finalizando TODO

--- curso-react-redux-master/todo-app-meu/frontend/src/todo/todoReducer.js
+++ curso-react-redux-master/todo-app-meu/frontend/src/todo/todoReducer.js
@@ -1,25 +1,15 @@
 const INITIAL_STATE = {
-    description: 'Ler Livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Reunião com a equipe às 10:00',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Consulta médica',
-        done: false
-    }]
+    description: '', list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return {...state, description: action.payload}
-    
+        case 'TODO_SEARCHED':
+            return {...state, list: action.payload}
+        case 'TODO_CLEAR':
+            return {...state, description: ''}
         default:
             return state;
     }
