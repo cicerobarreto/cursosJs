5b88cefdd3d7e7a986c5618375eee5ae937730fb
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-07-31 13:22:59 -0300

    dashboard com redux

--- curso-react-redux-master/meu-dindin/frontend/src/main/reducers.js
+++ curso-react-redux-master/meu-dindin/frontend/src/main/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
-    dashboard: () => ({summary: {credito: 100, debt: 50}})
+    dashboard: () => ({summary: {credit: 100, debt: 50}})
 })
 
 export default rootReducer
