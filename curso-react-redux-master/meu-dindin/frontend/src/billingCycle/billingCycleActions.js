7a74d3ec7478eeba647d3ea038f5d04259a881bc
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-08-03 17:09:55 -0300

    messagens

--- curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycleActions.js
+++ curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -12,6 +12,14 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação Realizado com sucesso.')
+        })
+        .catch(e => {
+            // e.response.data.erros.forEach(error => toastr.error('Error', error))
+            console.log(e.response.data.erros);
+            
+        })
     return {
         type: 'TEMP'
     }
