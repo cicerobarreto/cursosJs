11d958945dfca0f790fa2dbd356958986c01432c
cicero.barreto <cicero.barreto@dataprev.gov.br>
2018-08-03 08:30:48 -0300

    Inclusão BC parte 1

--- curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycle.jsx
+++ curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycle.jsx
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from '../billingCycle/billingCycleActions'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -39,7 +40,9 @@ class BillingCycle extends Component {
                             <TabContent id='tabList'> 
                                 <List />
                             </TabContent>
-                            <TabContent id='tabCreate'> <Form /> </TabContent>                                
+                            <TabContent id='tabCreate'> 
+                                <Form onSubmit={this.props.create} /> 
+                            </TabContent>                                
                             <TabContent id='tabUpdate'> <Form /> </TabContent>
                             <TabContent id='tabDelete'> <h1>Deletar</h1> </TabContent>
                         </TabsContent>
@@ -51,5 +54,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs,create}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
