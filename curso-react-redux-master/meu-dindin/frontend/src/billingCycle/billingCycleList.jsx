a1fed2ed4910631a5d8a19d9dd2a3b5577528a56
cicerobarreto@gmail.com <cicerobarreto@gmail.com>
2018-08-06 19:08:16 -0300

    Exibir aba de alterar BC

--- curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycleList.jsx
+++ curso-react-redux-master/meu-dindin/frontend/src/billingCycle/billingCycleList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { getList } from '../billingCycle/billingCycleActions'
+import { getList, showUpdate } from '../billingCycle/billingCycleActions'
 
 
 
@@ -19,6 +19,12 @@ class BillingCycleList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
+                <td>
+                    <button className='btn btn-warning'
+                     onClick={() => this.props.showUpdate(bc)}>
+                     <i className='fa fa-pencil'></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -31,7 +37,8 @@ class BillingCycleList extends Component {
                         <tr>
                             <th>Nome</th>
                             <th>Mês</th>
-                            <th>Ano</th>                            
+                            <th>Ano</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -44,5 +51,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycleReducer.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
