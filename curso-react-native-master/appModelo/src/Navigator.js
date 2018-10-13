478e80465f11ab1b8d5ab2d630f5d314f8f7820e
Cicero Barreto <cicerobarreto@gmail.com>
2018-10-13 11:00:05 -0300

    Menu

--- curso-react-native-master/appModelo/src/Navigator.js
+++ curso-react-native-master/appModelo/src/Navigator.js
@@ -1,8 +1,57 @@
 import React from 'react'
-import {createSwitchNavigator} from 'react-navigation'
+import {createSwitchNavigator, createDrawerNavigator} from 'react-navigation'
 
 import Agenda from './screens/Agenda'
 import Auth from './screens/Auth'
+import commonsStyle from './commonStyles'
+
+const MenuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: props => <Agenda title='Hoje' daysAhead={0} {...props} />,
+        navigationOptions: {
+            title: 'Hoje'
+        }
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: props => <Agenda title='Amanhã' daysAhead={1} {...props} />,
+        navigationOptions: {
+            title: 'Amanhã'
+        }
+    },
+    Week: {
+        name: 'Week',
+        screen: props => <Agenda title='Semana' daysAhead={7} {...props} />,
+        navigationOptions: {
+            title: 'Semana'
+        }
+    },
+    Month: {
+        name: 'Month',
+        screen: props => <Agenda title='Mês' daysAhead={30} {...props} />,
+        navigationOptions: {
+            title: 'Mês'
+        }
+    }
+}
+
+const MenuConfig= {
+    initialRouteName: 'Today',
+    contentOptions: {
+        lableStyle: {
+            fontFamily: commonsStyle.fontFamily,
+            fontWeight: 'nomal',
+            fontSize: 20
+        },
+        activeLabelStyle: {
+            color: '#080',
+
+        }
+    }
+}
+
+const MenuNavigator = createDrawerNavigator(MenuRoutes, MenuConfig)
 
 const MainRoutes = {
     Auth: {
@@ -11,7 +60,7 @@ const MainRoutes = {
     },
     Home: {
         name: 'Home',
-        screen: Agenda 
+        screen: MenuNavigator
     }
 }
 
